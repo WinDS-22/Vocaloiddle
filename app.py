01c749d4c19954810c17e40efa9df1ca4b50eef6
winds <128177393+WinDS-22@users.noreply.github.com>
2024-11-24 20:11:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,5 @@ def leaderboard():
     return render_template("leaderboard.html", scores=scores)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto automáticamente
+    app.run(host="0.0.0.0", port=port)

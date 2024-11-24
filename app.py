76b62996ae1375f1109f3cf5d92577ee38745e6c
winds <128177393+WinDS-22@users.noreply.github.com>
2024-11-24 20:04:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Conexión a MongoDB
-MONGO_URI = "mongodb+srv://<username>:<password>@cluster0.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://windscraft22:QQIv872Rw6QfIMsq@cluster0.g6a8d.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(MONGO_URI)
 db = client["vocaloid_game"]
 songs_collection = db["songs"]
